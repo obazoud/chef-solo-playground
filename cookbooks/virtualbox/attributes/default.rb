e607a3b9dc084bf9ac2495641d093372d7a31494
Olivier Bazoud <olivier.bazoud@gmail.com>
2012-01-02 17:48:21 +0100

    virtualbox cookbook

---
+++ cookbooks/virtualbox/attributes/default.rb
@@ -0,0 +1,7 @@
+# Author:: Olivier Bazoud (<olivier.bazoud@gmail.com>)
+# Apache 2.0 license
+# Cookbook Name:: virtualbox
+# Attributes:: default
+
+# General settings
+default['virtualbox']['version'] = '4.1'
---
+++ cookbooks/virtualbox/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Olivier Bazoud"
+maintainer_email "olivier.bazoud@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures virutal box"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+depends          "apt"
+supports         "ubuntu"
