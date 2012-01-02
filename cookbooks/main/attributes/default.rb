8acffbaf5ef4467b53bf85425355c6582c06a9a0
Olivier Bazoud <olivier.bazoud@gmail.com>
2012-01-02 17:13:19 +0100

    main cookbook

---
+++ cookbooks/main/attributes/default.rb
@@ -0,0 +1,5 @@
+# Author:: Olivier Bazoud (<olivier.bazoud@gmail.com>)
+# Apache 2.0 license
+# Cookbook Name:: main
+# Attributes:: default
+
---
+++ cookbooks/main/recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: main
+# Recipe:: default
+#
+
+# main
+package "nautilus-dropbox"
+
+package "skype"
+
+package "gimp"
+
+package "dconf-tools"
+package "nautilus-open-terminal"
+
+package "tofrodos"
+package "dos2unix"
+
+package "gedit"
+
+package "elinks"
+
+# ubuntu scrollbars
+package "overlay-scrollbar" do
+  action :purge
+end
+
+package "liboverlay-scrollbar-0.2-0" do
+  action :purge
+end
+
