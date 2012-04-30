25c70b67d8c7bc9406199ccf8bc12b7ac95bb5ea
Olivier Bazoud <olivier.bazoud@gmail.com>
2012-04-30 17:41:44 +0200

    fix eclipse cookbook: use eclipe attributes

--- cookbooks/eclipse/recipes/default.rb
+++ cookbooks/eclipse/recipes/default.rb
@@ -8,8 +8,8 @@ include_recipe "java"
 package "tar"
 
 directory "#{node['eclipse']['dir']}" do
-  owner "#{node[:oh_my_zsh][:user]}"
-  group "#{node[:oh_my_zsh][:user]}"
+  owner "#{node[:eclipse][:user]}"
+  group "#{node[:eclipse][:user]}"
   action :create
 end
 
@@ -17,52 +17,52 @@ remote_file "#{node['eclipse']['dir']}/eclipse-#{node['eclipse']['version']}.tar
   source "#{node['eclipse']['url']}"
   mode "0644"
   checksum "#{node['eclipse']['checksum']}"
-  owner "#{node[:oh_my_zsh][:user]}"
-  group "#{node[:oh_my_zsh][:user]}"
+  owner "#{node[:eclipse][:user]}"
+  group "#{node[:eclipse][:user]}"
   not_if {File.exists?("#{node['eclipse']['dir']}/eclipse-#{node['eclipse']['version']}.tar.gz")}
 end
 
-execute "tar xzf #{node['eclipse']['dir']}/eclipse-#{node['eclipse']['version']}.tar.gz -C #{node['eclipse']['dir']} --owner #{node[:oh_my_zsh][:user]}" do
-  user "#{node[:oh_my_zsh][:user]}"
-  group "#{node[:oh_my_zsh][:user]}"
+execute "tar xzf #{node['eclipse']['dir']}/eclipse-#{node['eclipse']['version']}.tar.gz -C #{node['eclipse']['dir']} --owner #{node[:eclipse][:user]}" do
+  user "#{node[:eclipse][:user]}"
+  group "#{node[:eclipse][:user]}"
   not_if {File.directory?("#{node['eclipse']['dir']}/eclipse-#{node['eclipse']['version']}/")}
 end
 
 execute "mv #{node['eclipse']['dir']}/eclipse #{node['eclipse']['dir']}/eclipse-#{node['eclipse']['version']}" do
-  user "#{node[:oh_my_zsh][:user]}"
-  group "#{node[:oh_my_zsh][:user]}"
+  user "#{node[:eclipse][:user]}"
+  group "#{node[:eclipse][:user]}"
   not_if {File.directory?("#{node['eclipse']['dir']}/eclipse-#{node['eclipse']['version']}")}
 end
 
 link "#{node['eclipse']['dir']}/default" do
-  owner "#{node[:oh_my_zsh][:user]}"
-  group "#{node[:oh_my_zsh][:user]}"
+  owner "#{node[:eclipse][:user]}"
+  group "#{node[:eclipse][:user]}"
   to "#{node['eclipse']['dir']}/eclipse-#{node['eclipse']['version']}"
 end
 
 link "/usr/bin/eclipse" do
-  owner "#{node[:oh_my_zsh][:user]}"
-  group "#{node[:oh_my_zsh][:user]}"
+  owner "#{node[:eclipse][:user]}"
+  group "#{node[:eclipse][:user]}"
   to "#{node['eclipse']['dir']}/default/eclipse"
 end
 
-directory "/home/#{node[:oh_my_zsh][:user]}/.eclipse" do
-  owner "#{node[:oh_my_zsh][:user]}"
-  group "#{node[:oh_my_zsh][:user]}"
+directory "/home/#{node[:eclipse][:user]}/.eclipse" do
+  owner "#{node[:eclipse][:user]}"
+  group "#{node[:eclipse][:user]}"
   action :create
 end
 
-template "/home/#{node[:oh_my_zsh][:user]}/.eclipse/eclipserc" do
+template "/home/#{node[:eclipse][:user]}/.eclipse/eclipserc" do
   source "eclipserc.erb"
-  owner "#{node[:oh_my_zsh][:user]}"
-  group "#{node[:oh_my_zsh][:user]}"
+  owner "#{node[:eclipse][:user]}"
+  group "#{node[:eclipse][:user]}"
   mode "0755"
 end
 
 template "#{node['eclipse']['dir']}/eclipse-#{node['eclipse']['version']}/eclipse.ini" do
   source "eclipse.ini.erb"
-  owner "#{node[:oh_my_zsh][:user]}"
-  group "#{node[:oh_my_zsh][:user]}"
+  owner "#{node[:eclipse][:user]}"
+  group "#{node[:eclipse][:user]}"
   mode "0755"
 end
 
@@ -70,8 +70,8 @@ end
 package "gtk2-engines-pixbuf"
 
 #directory "#{node['eclipse']['dir']}/dropins" do
-#  owner "#{node[:oh_my_zsh][:user]}"
-#  group "#{node[:oh_my_zsh][:user]}"
+#  owner "#{node[:eclipse][:user]}"
+#  group "#{node[:eclipse][:user]}"
 #  action :create
 #end
 
@@ -80,8 +80,8 @@ package "gtk2-engines-pixbuf"
 #    source "#{node['eclipse']['plugin'][plugin]['url']}"
 #    mode "0644"
 #    checksum "#{node['eclipse']['plugin'][plugin]['checksum']}"
-#    owner "#{node[:oh_my_zsh][:user]}"
-#    group "#{node[:oh_my_zsh][:user]}"
+#    owner "#{node[:eclipse][:user]}"
+#    group "#{node[:eclipse][:user]}"
 #    not_if {File.exists?("#{node['eclipse']['dir']}/dropins/#{node['eclipse']['plugin'][plugin]['file']}")}
 #  end
 #end
