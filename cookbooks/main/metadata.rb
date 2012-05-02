1c362e79b4f8ccaf83724223eebd2c899c142d76
Olivier Bazoud <olivier.bazoud@gmail.com>
2012-05-02 17:51:05 +0200

    Add sublime text 2

--- cookbooks/main/metadata.rb
+++ cookbooks/main/metadata.rb
@@ -4,7 +4,7 @@ license          "Apache 2.0"
 description      "Installs/Configures stuff"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
-
+depends          "apt"
 %w( ubuntu ).each do |os|
   supports os
 end
