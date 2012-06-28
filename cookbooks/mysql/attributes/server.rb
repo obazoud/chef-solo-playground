bb22b7540bfb09043cd40d279107b79d1802230c
Olivier Bazoud <olivier.bazoud@gmail.com>
2012-06-28 08:55:40 +0200

    updating mysql cookbooks

--- cookbooks/mysql/attributes/server.rb
+++ cookbooks/mysql/attributes/server.rb
@@ -67,6 +67,13 @@ when "windows"
   default['mysql']['conf_dir']                = "#{mysql['basedir']}"
   default['mysql']['old_passwords']           = 0
   default['mysql']['grants_path']             = "#{mysql['conf_dir']}\\grants.sql"
+when "mac_os_x"
+  default['mysql']['package_name']            = "mysql"
+  default['mysql']['basedir']                 = "/usr/local/Cellar"
+  default['mysql']['data_dir']                = "/usr/local/var/mysql"
+  default['mysql']['root_group']              = "admin"
+  default['mysql']['mysqladmin_bin']          = "/usr/local/bin/mysqladmin"
+  default['mysql']['mysql_bin']               = "/usr/local/bin/mysql"
 else
   default['mysql']['package_name']            = "mysql-server"
   default['mysql']['service_name']            = "mysql"
@@ -90,6 +97,11 @@ if attribute?('ec2')
   default['mysql']['ebs_vol_size'] = 50
 end
 
+default['mysql']['use_upstart'] = platform?("ubuntu") && node.platform_version.to_f >= 10.04
+
+default['mysql']['auto-increment-increment']        = 1
+default['mysql']['auto-increment-offset']           = 1
+
 default['mysql']['allow_remote_root']               = false
 default['mysql']['tunable']['back_log']             = "128"
 default['mysql']['tunable']['key_buffer']           = "256M"
@@ -107,6 +119,27 @@ default['mysql']['tunable']['thread_concurrency']   = 10
 default['mysql']['tunable']['thread_stack']         = "256K"
 default['mysql']['tunable']['wait_timeout']         = "180"
 
+default['mysql']['tunable']['log_bin']                         = nil
+default['mysql']['tunable']['log_bin_trust_function_creators'] = false
+default['mysql']['tunable']['relay_log']                       =  nil
+default['mysql']['tunable']['log_slave_updates']               = false
+default['mysql']['tunable']['sync_binlog']                     = 0
+default['mysql']['tunable']['skip_slave_start']                = false
+
+default['mysql']['tunable']['log_error']                       = nil
+default['mysql']['tunable']['log_queries_not_using_index']     = true
+default['mysql']['tunable']['log_bin_trust_function_creators'] = false
+
+default['mysql']['tunable']['innodb_buffer_pool_size']         = "128M"
+default['mysql']['tunable']['innodb_log_file_size']            = "5M"
+default['mysql']['tunable']['innodb_buffer_pool_size']         = "128M"
+default['mysql']['tunable']['innodb_additional_mem_pool_size'] = "8M"
+default['mysql']['tunable']['innodb_data_file_path']           = "ibdata1:10M:autoextend"
+default['mysql']['tunable']['innodb_flush_log_at_trx_commit']  = "1"
+default['mysql']['tunable']['innodb_flush_method']             = "fdatasync"
+default['mysql']['tunable']['innodb_log_buffer_size']          = "8M"
+default['mysql']['tunable']['innodb_adaptive_flushing']        = "true"
+
 default['mysql']['tunable']['query_cache_limit']    = "1M"
 default['mysql']['tunable']['query_cache_size']     = "16M"
 
@@ -115,5 +148,3 @@ default['mysql']['tunable']['long_query_time']      = 2
 
 default['mysql']['tunable']['expire_logs_days']     = 10
 default['mysql']['tunable']['max_binlog_size']      = "100M"
-
-default['mysql']['tunable']['innodb_buffer_pool_size']  = "256M"
